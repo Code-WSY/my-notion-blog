678a04ab38c8ac8f654e2f41f8cb35de152464f9
suyun <sylearn@foxmail.com>
2024-07-01 18:23:46 +0800

    chore: Update social media links in PageActions component

--- components/PageActions.tsx
+++ components/PageActions.tsx
@@ -13,20 +13,24 @@ export const PageActions: React.FC<{ tweet: string }> = ({ tweet }) => {
     <div className={styles.pageActions}>
       <a
         className={styles.likeTweet}
-        href={`https://twitter.com/intent/like?tweet_id=${tweet}`}
+        //href={`https://twitter.com/intent/like?tweet_id=${tweet}`}
+        href={`https://zhuanlan.zhihu.com/p/${tweet}`}
         target='_blank'
         rel='noopener noreferrer'
-        title='Like this post on Twitter'
+        //title='Like this post on Twitter'
+        title='Like this post on Zhihu'
       >
         <IoHeartOutline />
       </a>
 
       <a
         className={styles.retweet}
-        href={`https://twitter.com/intent/retweet?tweet_id=${tweet}`}
+        //href={`https://twitter.com/intent/retweet?tweet_id=${tweet}`}
+        href={`https://zhuanlan.zhihu.com/p/${tweet}`}
         target='_blank'
         rel='noopener noreferrer'
-        title='Retweet this post on Twitter'
+        //title='Retweet this post on Twitter'
+        title='Retweet this post on Zhihu'
       >
         <AiOutlineRetweet />
       </a>
