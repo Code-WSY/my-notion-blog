f01bba936994fb12793c5fc0ce58192e25dcbd47
suyun <sylearn@foxmail.com>
2024-07-02 20:08:21 +0800

    chore: Update site.config.ts with custom navigation links

--- site.config.ts
+++ site.config.ts
@@ -3,11 +3,9 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   rootNotionPageId: '70977a748819466685852151e8eb61fa',
-
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
-
   // basic site info (required)
   name: 'Sylearn Blog',
   domain: 'fire-lyric-01e.notion.site',
